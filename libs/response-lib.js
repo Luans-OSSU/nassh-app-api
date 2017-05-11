e957764d72b3190327a5d6346162c6cf48a5bf0d
Luans <luansnain@gmail.com>
2017-05-11 22:24:13 +0200

    setup

---
+++ libs/response-lib.js
@@ -0,0 +1,18 @@
+export function succes(body) {
+    return buildResponse(200, body);
+}
+
+export function failure(body) {
+    return buildResponse(500, body);
+}
+
+function buildResponse(statusCode, body) {
+    return {
+        statusCode: statusCode,
+        header: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(body),
+    };
+}
